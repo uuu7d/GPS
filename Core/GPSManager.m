c29f1f6c249297e6d68a25ee950e5558f4d76529
Raed / Dev-land <rrxs511@gmail.com>
2025-12-29 06:42:32 +0300

    Refactor key window retrieval for gesture registration

--- Core/GPSManager.m
+++ Core/GPSManager.m
@@ -25,15 +25,37 @@
     return self.spoofingEnabled;
 }
 
+#pragma mark - Window Helper (iOS 13+ / iOS 18)
+
+- (UIWindow *)activeKeyWindow {
+    for (UIScene *scene in UIApplication.sharedApplication.connectedScenes) {
+        if (scene.activationState != UISceneActivationStateForegroundActive) {
+            continue;
+        }
+
+        if (![scene isKindOfClass:[UIWindowScene class]]) {
+            continue;
+        }
+
+        UIWindowScene *windowScene = (UIWindowScene *)scene;
+        for (UIWindow *window in windowScene.windows) {
+            if (window.isKeyWindow) {
+                return window;
+            }
+        }
+    }
+    return nil;
+}
+
 #pragma mark - Gesture
 
 - (void)registerGesture {
-    UIWindow *window = UIApplication.sharedApplication.keyWindow;
+    UIWindow *window = [self activeKeyWindow];
     if (!window) return;
 
     UILongPressGestureRecognizer *gesture =
     [[UILongPressGestureRecognizer alloc] initWithTarget:self
-                                                    action:@selector(handleGesture:)];
+                                                  action:@selector(handleGesture:)];
 
     gesture.minimumPressDuration = 5.0;
     gesture.numberOfTouchesRequired = 2;
